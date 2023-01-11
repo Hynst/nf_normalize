3441df1c2152a8feb6e5bad7b05e83a7ed3b20df
user <user@bibs2-0.bibs2.hynst-ns.svc.cluster.local>
2023-01-11 14:09:45 +0100

    create vcf configs

---
+++ normalize_create_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+vcfs=$1
+run_id=$2
+
+touch ${run_id}_config_vcfs.tsv
+
+for vcf in `ls $vcfs | grep -v "tbi"`
+do
+
+tmp=${vcf#*_}
+s_id=${tmp%_*}
+file=${vcfs}/${vcf}
+
+awk -v S_ID="$s_id" -v file="$file" 'BEGIN{print S_ID, file}' | sed 's/ /\t/g' >> ${run_id}_config_vcfs.tsv
+
+done
